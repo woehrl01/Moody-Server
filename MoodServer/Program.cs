ed0c56198e5a83f608fc082e1f2b5cf1921b6383
Azer0s <sim17057@spengergasse.at>
2016-06-30 21:43:04 +0200

    Initial commit

---
+++ MoodServer/Program.cs
@@ -0,0 +1,202 @@
+﻿using Nancy.Hosting.Self;
+using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace MoodServer{
+
+    public class MoodModule : Nancy.NancyModule
+    {
+
+        private DBManager db;
+
+        public MoodModule()
+        {
+            db = new DBManager();
+
+            Get["/"] = _ =>
+            {
+                return "Test";
+            };
+
+            Get["/api/info"] = _ =>
+            {
+                return "<b>Moody by Ariel Simulevski</b><br>Server powered by <b>NancyFX</b><br>Client powered by <b>Xamarin</b>";
+            };
+
+            Get["/api/entry/{mood}&{location}"] = parameters =>
+            {
+                int mood = parameters.mood;
+                int location = parameters.location;
+                db.saveMood(mood, location);
+                return mood.ToString() + " " + location;
+            };
+
+            Get["/api/locations/"] = _ =>
+            {
+                return db.getLocations();
+            };
+        }
+    }
+
+    class Program
+    {
+        private string _url = "http://localhost";
+        private int _port = 80;
+        
+
+        private void Start()
+        {
+            var uri = new Uri($"{_url}:{_port}/");
+            using (var nancy = new NancyHost(uri))
+            {
+                try
+                {
+                    nancy.Start();
+                    Console.WriteLine($"Started listennig port on {_port}");
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                }
+                Console.ReadKey();
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            var p = new Program();
+            p.Start();
+        }
+    }
+
+    class DBManager
+    {
+        public SqlConnection connection;
+
+        public DBManager()
+        {
+            String connectionString = "Data Source=PROTEUSIV\\SQLEXPRESS;Initial Catalog=mood;Integrated Security=True;Connect Timeout=15;Encrypt=False;TrustServerCertificate=True;ApplicationIntent=ReadWrite;MultiSubnetFailover=False";
+            connection = new SqlConnection(connectionString);
+
+            try
+            {
+                connection.Open();
+                connection.Close();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Can not open connection to SQL Server ! ");
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void saveMood(int mood, int location)
+        {
+            DateTime localDate = DateTime.Now;
+            Console.WriteLine(localDate.ToString());
+            String guid = Guid.NewGuid().ToString(); ;
+
+            String save = "INSERT into entries (Id,mood,location,date) VALUES (@id,@mood,@loc,@date)";
+
+            using (SqlCommand command = new SqlCommand(save))
+            {
+                command.Connection = connection;
+                command.Parameters.Add("@id", SqlDbType.VarChar, 36).Value = guid;
+                command.Parameters.Add("@mood", SqlDbType.Int).Value = mood;
+                command.Parameters.Add("@loc", SqlDbType.Int).Value = location;
+                command.Parameters.Add("@date", SqlDbType.DateTime).Value = localDate;
+
+                try
+                {
+                    connection.Open();
+                    Console.WriteLine("SQL Connection Open ! ");
+                    command.ExecuteNonQuery();
+                    Console.WriteLine("Inserting data... ");
+
+                    string query = command.CommandText;
+
+                    foreach (SqlParameter p in command.Parameters)
+                    {
+                        query = query.Replace(p.ParameterName, p.Value.ToString());
+                    }
+                    Console.WriteLine("[" + query + "]");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Something went wrong ! ");
+                    Console.WriteLine(e.Message);
+                }
+                finally
+                {
+                    connection.Close();
+                    Console.WriteLine("SQL Connection Closed ! ");
+                }
+            }
+        }
+
+        public String getLocations()
+        {
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            try
+            {
+                connection.Open();
+                Console.WriteLine("SQL Connection Open ! ");
+                cmd = new SqlCommand("SELECT Id,location FROM locations", connection);
+                reader = cmd.ExecuteReader();
+                Console.WriteLine("Getting locations... ");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong ! ");
+                Console.WriteLine(e.Message);
+            }
+
+            List<Loc> loc = new List<Loc>();
+
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    loc.Add(new Loc(reader.GetInt32(0), reader.GetString(1)));
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+            }
+            connection.Close();
+            Console.WriteLine("SQL Connection Closed ! ");
+
+            String json = JsonConvert.SerializeObject(loc, Formatting.Indented);
+            return json;
+        }
+    }
+
+    public class Loc
+    {
+        private int id;
+        private string location;
+
+        public Loc(int id, string location)
+        {
+            this.Identiefier = id;
+            this.Location = location;
+        }
+
+        public string Location
+        {
+            get { return location; }
+            set { location = value; }
+        }
+
+        public int Identiefier
+        {
+            get { return id; }
+            set { id = value; }
+        }
+    }
+}
