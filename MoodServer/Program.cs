d82c60ac5c12be36ae0c720528cfa4d7147af647
Azer0s <sim17057@spengergasse.at>
2016-07-05 15:48:30 +0200

    Added diagram

--- MoodServer/Program.cs
+++ MoodServer/Program.cs
@@ -7,10 +7,10 @@ using System.Collections.Generic;
 using System.Data;
 using System.Data.SqlClient;
 using System.IO;
+using System.Net;
 
 namespace MoodServer
 {
-
     public class MoodModule : Nancy.NancyModule
     {
 
@@ -20,10 +20,35 @@ namespace MoodServer
         {
             db = new DBManager();
 
-            Get["/"] = _ =>
+            Get[@"/"] = parameters =>
             {
-                //TODO add chart page
-                return "";
+                WebClient client = new WebClient();
+                return client.DownloadString("views/form.html");
+            };
+
+            Post["/results/diagram"] = _ =>
+            {
+                string loc = Request.Form.location;
+                string day = Request.Form.day;
+                string month = Request.Form.month;
+                string year = Request.Form.year;
+                WebClient client = new WebClient();
+                return client.DownloadString("views/dia.html") + "<script src='/api/diagramscript/" + loc + "&" + day + "&" + month + "&" + year + "'></script></html>";
+            };
+
+            Get["/api/diagramscript/{loc}&{day}&{month}&{year}"] = parameters =>
+            {
+                return db.MakeDiagramScript(parameters.loc,parameters.month + "/" + parameters.day + "/" + parameters.year);
+            };
+
+            Get["/api/locations/"] = _ =>
+            {
+                return db.GetLocations();
+            };
+
+            Get["/api/locationscript"] = _ =>
+            {
+                return db.MakeSelectScript();
             };
 
             Get["/api/info"] = _ =>
@@ -31,17 +56,12 @@ namespace MoodServer
                 return "<b>Moody by Ariel Simulevski</b><br>Server powered by <b>NancyFX</b><br>Client powered by <b>Xamarin</b>";
             };
 
-            Get["/api/entry/{mood}&{location}"] = parameters =>
+            Post["/api/entry"] = _ =>
             {
-                int mood = parameters.mood;
-                int location = parameters.location;
+                int mood = Request.Form.mood;
+                int location = Request.Form.location;
                 db.SaveMood(mood, location);
-                return mood.ToString() + " " + location;
-            };
-
-            Get["/api/locations/"] = _ =>
-            {
-                return db.GetLocations();
+                return "";
             };
         }
     }
@@ -55,12 +75,12 @@ namespace MoodServer
             _rootPathProvider = rootPathProvider;
         }
 
-        public bool HandlesStatusCode(HttpStatusCode statusCode, NancyContext context)
+        public bool HandlesStatusCode(Nancy.HttpStatusCode statusCode, NancyContext context)
         {
-            return statusCode == HttpStatusCode.NotFound;
+            return statusCode == Nancy.HttpStatusCode.NotFound;
         }
 
-        public void Handle(HttpStatusCode statusCode, NancyContext context)
+        public void Handle(Nancy.HttpStatusCode statusCode, NancyContext context)
         {
             context.Response.Contents = stream =>
             {
@@ -188,22 +208,153 @@ namespace MoodServer
 
             List<Loc> loc = new List<Loc>();
 
-            if (reader.HasRows)
+            if(reader != null)
             {
-                while (reader.Read())
+                if (reader.HasRows)
                 {
-                    loc.Add(new Loc(reader.GetInt32(0), reader.GetString(1)));
+                    while (reader.Read())
+                    {
+                        loc.Add(new Loc(reader.GetInt32(0), reader.GetString(1)));
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("No rows found.");
                 }
             }
             else
             {
-                Console.WriteLine("No rows found.");
+                Console.WriteLine("Something went wrong ! ");
+            }
+            
+            connection.Close();
+            Console.WriteLine("SQL Connection Closed ! ");
+
+            string json = null;
+            try
+            {
+                json = JsonConvert.SerializeObject(loc, Formatting.Indented);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("Something went wrong ! ");
+            }
+            return json;
+        }
+
+        public string MakeSelectScript()
+        {
+            string locations = GetLocations();
+            List<Loc> locationList = JsonConvert.DeserializeObject<List<Loc>>(locations);
+            List<string> locs = new List<string>();
+
+            foreach (Loc l in locationList)
+            {
+                locs.Add(l.Location);
+            }
+
+            string script = "var x = document.getElementById('locations');";
+
+            foreach (string s in locs)
+            {
+                script += "var option = document.createElement('option');option.text ='" + s + "';x.add(option); ";
+            }
+            script += "$('#day_1').val('" + DateTime.Today.Day.ToString() + "');";
+            script += "$('#month_1').val('" + DateTime.Today.Month.ToString() + "');";
+            script += "$('#year_1').val('" + DateTime.Today.Year.ToString() + "');";
+            return script;
+        }
+
+        public string MakeDiagramScript(string loc,string date)
+        {
+            string script = "$('#title').text('" + loc + " - " + date + "');" + GetEntries(GetIDByName(loc).ToString(), date) + "Plotly.newPlot('diagram', data);";
+            return script;
+        }
+
+        public int GetIDByName(string loc)
+        {
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            int id = 0;
+            try
+            {
+                connection.Open();
+                Console.WriteLine("SQL Connection Open ! ");
+                cmd = new SqlCommand("SELECT Id FROM locations WHERE location = '" + loc + "'", connection);
+                reader = cmd.ExecuteReader();
+                Console.WriteLine("Getting locationID from location " + loc + "...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong ! ");
+                Console.WriteLine(e.Message);
+            }
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    id = reader.GetInt32(0);
+                }
             }
             connection.Close();
             Console.WriteLine("SQL Connection Closed ! ");
 
-            String json = JsonConvert.SerializeObject(loc, Formatting.Indented);
-            return json;
+            return id;
+        }
+
+        public string GetEntries(string locationId, string date)
+        {
+            SqlCommand cmd;
+            SqlDataReader reader = null;
+            List<string> x = new List<string>();
+            List<string> y = new List<string>();
+            try
+            {
+                connection.Open();
+                Console.WriteLine("SQL Connection Open ! ");
+                cmd = new SqlCommand("select m.[Desc],count(e.mood) from mood m left join entries e on e.mood = m.Id and e.location =" + locationId + "and CONVERT(DATETIME, FLOOR(CONVERT(FLOAT, e.[date]))) = '" + date +"' group by  m.[Desc], mood, m.id order by m.Id asc", connection);
+                reader = cmd.ExecuteReader();
+                Console.WriteLine("Getting entries for date " + date + " for locationID " + locationId + "...");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Something went wrong ! ");
+                Console.WriteLine(e.Message);
+            }
+            if (reader.HasRows)
+            {
+                while (reader.Read())
+                {
+                    x.Add("'" + reader.GetString(0) + "'");
+                    y.Add(reader.GetInt32(1).ToString());
+                    Console.WriteLine(reader.GetString(0) + " - " + reader.GetInt32(1));
+                }
+            }
+
+            string data = "var data = [{" + ToCoordinateString(x, "x") + ToCoordinateString(y, "y") + "type: 'bar'}];";
+            connection.Close();
+            Console.WriteLine("SQL Connection Closed ! ");
+            return data;
+        }
+
+        public string ToCoordinateString(List<string> coordAsList, string type)
+        {
+            string xy = type + ": [";
+            bool set = false;
+            foreach (string loc in coordAsList)
+            {
+                if (set)
+                {
+                    xy += "," + loc;
+                }
+                else
+                {
+                    xy += loc;
+                    set = true;
+                }
+            }
+            xy += "],";
+            return xy;
         }
     }
 
